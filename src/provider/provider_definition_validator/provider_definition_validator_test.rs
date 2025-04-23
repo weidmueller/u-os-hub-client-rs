b1d8cb473cf58ac7ebc72e21596ea5c94142b702
Philipp Meyer <Philipp.Meyer@weidmueller.com>
2025-04-23 12:54:04 +0000

    feat: add unit test for testing validator performance

--- src/provider/provider_definition_validator/provider_definition_validator_test.rs
+++ src/provider/provider_definition_validator/provider_definition_validator_test.rs
@@ -1,4 +1,9 @@
-use crate::provider::test_data;
+use crate::{
+    generated::weidmueller::ucontrol::hub::{
+        VariableAccessType, VariableDataType, VariableDefinitionT,
+    },
+    provider::test_data,
+};
 
 use super::*;
 use rstest::rstest;
@@ -27,4 +32,35 @@ fn test_validate(
     #[case] expected_result: Result<(), InvalidProviderDefinitionError>,
 ) {
     assert_eq!(provider_definition.validate(), expected_result);
+}
+
+#[rstest]
+fn test_performance() {
+    //Create a provider definition with 50k variables
+    let num_vars = 50_000u32;
+    let mut provider_definition = ProviderDefinitionT::default();
+
+    let mut variable_definitions = Vec::with_capacity(num_vars as usize);
+    for i in 0..num_vars {
+        let variable_definition = VariableDefinitionT {
+            id: i,
+            key: format!("var_string.my_variable{i}"),
+            data_type: VariableDataType::STRING,
+            access_type: VariableAccessType::READ_WRITE,
+            experimental: false,
+        };
+        variable_definitions.push(variable_definition);
+    }
+
+    provider_definition.variable_definitions = Some(variable_definitions);
+
+    // Measure the time it takes to validate the provider definition
+    let start_time = std::time::Instant::now();
+    provider_definition.validate().unwrap();
+    let duration = start_time.elapsed();
+    println!("Validation took: {:?}", duration);
+
+    //Not strictly deterministic (hardware dependent), but this should be reasonable for debug builds
+    //Currently only takes about 100ms on my local hardware.
+    assert!(duration.as_secs() < 2, "Validation took too long");
 }
