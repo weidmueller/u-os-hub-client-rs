e6f118a005030f18f62d2a31a3b440c3c04f1b1d
Philipp Meyer <Philipp.Meyer@weidmueller.com>
2025-04-23 09:44:29 +0000

    feat: add rustdoc examples and refinements for consumer part

--- src/consumer/mod.rs
+++ src/consumer/mod.rs
@@ -1,4 +1,78 @@
 //! This module provides APIs and data types for interacting with the u-OS Data Hub as a consumer.
+//!
+//! The consumer API is split into a low- and a high-level API.
+//!
+//! The high-level API is designed for ease of use and provides a more user-friendly interface.
+//! It is recommended to use the high-level API unless you have very specific performance requirements or need to access advanced features.
+//!
+//! The low-level API is designed for advanced users who want to have full control and performance, at the cost of lower usability.
+//! It may change at any time without compatibility guarantees and uses raw NATS and flatbuffer data types without abstraction.
+//!
+//! All modules and structs with "DataHub / dh" in their name are part of the high-level API, while "Nats" indicates the low-level API.
+//! Please refer to the module and struct documentation for more details on the available APIs and data types.
+//!
+//! The following example demonstrates how to connect to a provider and read and write variables via the high-level API:
+//!
+//! ```no_run
+//!# use std::sync::Arc;
+//! //The prelude contains all required imports for the consumer API
+//! use u_os_hub_client::prelude::consumer::*;
+//!
+//! #[tokio::main]
+//! async fn main() -> anyhow::Result<()> {
+//!     //The provider id to connect to
+//!     let provider_id = "test-provider";
+//!     
+//!     //Configure your nats server authentication
+//!     let auth_settings =
+//!         AuthenticationSettingsBuilder::new(NatsPermission::VariableHubReadWrite)
+//!            .with_credentials(OAuth2Credentials {
+//!                //NATS client name of the consumer
+//!                client_name: "test-consumer".to_string(),
+//!                //Obtained by the uOS Identity&Access Client GUI
+//!                client_id: "<your_oauth_client_id>".to_string(),
+//!                client_secret: "<your_oauth_client_secret>".to_string(),
+//!            })
+//!            .build();
+//!
+//!     //Create consumer
+//!     let dh_consumer = Arc::new(
+//!         DataHubConsumer::connect(DEFAULT_U_OS_NATS_ADDRESS, &auth_settings)
+//!         .await?,
+//!     );
+//!
+//!     //Connect to a provider
+//!     println!("Trying to connect to provider {provider_id:?} ...");
+//!     let dh_provider_con = ConnectedDataHubProvider::new(dh_consumer, provider_id,
+//!         true).await?;
+//!
+//!     //Print all variable ids, their definition and their values
+//!     println!("Variable overview:");
+//!     for def in dh_provider_con.get_all_variable_definitions()? {
+//!         let var_key = &def.key;
+//!         let val = dh_provider_con.read_single_variable(var_key).await?;
+//!         println!("\t{var_key}:");
+//!         println!("\t\tDefinition: {def:?}");
+//!         println!("\t\tValue: {val:?}");
+//!     }
+//!
+//!     //Explicitly creating a variable key once and reusing it
+//!     //multiple times improves performance
+//!     let written_var_handle1 = VariableKey::from("folder2.writable_string");
+//!
+//!     //write multiple variables at once
+//!     let var_changes = [
+//!         (written_var_handle1, ConsumerVariableValue::from("Multi write!!!")),
+//!         //DataHub types usually implement the From trait,
+//!         //so you can use .into() for values and keys
+//!         ("folder2.writable_int".into(), 123.into()),
+//!     ];
+//!     dh_provider_con.write_variables(&var_changes).await?;
+//!
+//!     Ok(())
+//! }
+//!
+//! ```
 
 pub mod connected_dh_provider;
 pub mod connected_nats_provider;
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
-//! This is the internal u-OS Data Hub client library.
-//! The focus of this library is high performance.
+//! Welcome to the u-OS Data Hub client library.
 //!
-//! This library primary contains:
+//! This library allows to interact with the uOS Variable Data Hub as a provider or consumer.
 //!
-//! - Provider Handling
-//! - Consumer Handling
-//! - The generated flatbuffers of the Variable-NATS-API
-//!
+//! As a starting point, please take a look at the [provider] and [consumer] module documentation.
+//! They provide example code and a detailed description of the available APIs.
 
 // The module comment/description is located here to not change the generated files.
 // Because of the same reason the "dead_code", "unused_imports" and "clippy::all" is allowed for the generated module.
@@ -30,7 +27,10 @@ pub mod variable_value_type_serde_extension;
 pub mod prelude {
     /// Common imports for data hub consumers.
     pub mod consumer {
-        pub use crate::authenticated_nats_con::AuthenticatedNatsConnection;
+        pub use crate::authenticated_nats_con::{
+            AuthenticatedNatsConnection, AuthenticationSettings, AuthenticationSettingsBuilder,
+            NatsPermission, DEFAULT_U_OS_NATS_ADDRESS,
+        };
         pub use crate::consumer::connected_dh_provider::{
             ConnectedDataHubProvider, ProviderEvent, VariableKeyLike,
         };
@@ -41,13 +41,18 @@ pub mod prelude {
             ConsumerVariableType, DhRegistryState,
         };
         pub use crate::consumer::variable_key::VariableKey;
+        pub use crate::oauth2::OAuth2Credentials;
         pub use crate::variable::value::Value as ConsumerVariableValue;
         pub use crate::variable::value::{DhDuration, DhTimestamp};
     }
 
     /// Common imports for data hub providers.
     pub mod provider {
-        pub use crate::authenticated_nats_con::AuthenticatedNatsConnection;
+        pub use crate::authenticated_nats_con::{
+            AuthenticatedNatsConnection, AuthenticationSettings, AuthenticationSettingsBuilder,
+            NatsPermission, DEFAULT_U_OS_NATS_ADDRESS,
+        };
+        pub use crate::oauth2::OAuth2Credentials;
         pub use crate::provider::{Provider, ProviderOptions, VariableBuilder};
         pub use crate::variable::value::{DhDuration, DhTimestamp};
     }
