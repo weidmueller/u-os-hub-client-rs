6e4c88d0813fd8b5f258dff9bc94d5f7f0c76c81
Philipp Meyer <Philipp.Meyer@weidmueller.com>
2025-05-19 08:45:02 +0000

    fix: nats server installation, add check to start script

--- tools/start-nats-server.sh
+++ tools/start-nats-server.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 set -euo pipefail
-set -x
 
+# Check for invalid, e.g. empty, downloads
+# This may happen if a nats version gets yanked or is taken offline
+# Unfortunately, the nats download script does not properly handle this
+if [[ ! -s "$(command -v nats-server)" || ! -x "$(command -v nats-server)" ]]; then
+    echo "Error: nats-server is not found, not executable, or is empty. Validate download."
+    exit 1
+fi
+
+set -x
 nats-server -a 127.0.0.1 -p 4222
